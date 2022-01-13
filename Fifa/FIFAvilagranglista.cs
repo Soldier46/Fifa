a50f46ce8f993806904576f1109da09cc4585795
Soldier46 <rschuszler46@gmail.com>
2022-01-13 09:45:23 +0100

    3.feladat xd

--- Fifa/FIFAvilagranglista.cs
+++ Fifa/FIFAvilagranglista.cs
@@ -3,16 +3,20 @@
 namespace Fifa
 {
     class Program
-    {
-        static void Main(string[] args)
-        {
-            public string Csapat { get; set; }
+    { 
+        public string Csapat { get; set; }
            public int Helyezés { get; set; }
         public int Valtozás { get; set; }
         public int Pontszám { get; set; }
 
-
-
+        static void Main(string[] args)
+        {
+               List<Csapat> Csapatok = new List<Csapat>();
+            foreach (var sor in File.ReadAllLines("fifa.txt").Skip(1))
+            {
+                Csapatok.Add(new Csapat(sor));
+            }
+             Console.WriteLine($"3.feladat: A fifázgatásban  {Csapatok.Count} csapat szerepel.");
         }
     }
 }
