c86f6e106ca43758acaad7ec3079f47d3c7aa86d
Soldier46 <rschuszler46@gmail.com>
2022-01-13 09:27:15 +0100

    Add project files.

---
+++ Fifa/FIFAvilagranglista.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Fifa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public string Csapat { get; set; }
+           public int Helyezés { get; set; }
+        public int Valtozás { get; set; }
+        public int Pontszám { get; set; }
+        }
+    }
+}
