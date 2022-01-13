a7e78b370ddf091e12c6fb3cfa1a09359bb24318
Soldier46 <rschuszler46@gmail.com>
2022-01-13 10:52:15 +0100

    1-7.feladat

--- Fifa/FIFAvilagranglista.cs
+++ Fifa/FIFAvilagranglista.cs
@@ -1,22 +1,58 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
 
 namespace Fifa
 {
     class Program
     { 
-        public string Csapat { get; set; }
-           public int Helyezés { get; set; }
-        public int Valtozás { get; set; }
-        public int Pontszám { get; set; }
-
+       
         static void Main(string[] args)
         {
-               List<Csapat> Csapatok = new List<Csapat>();
+        
+            List<csapat> csapatok = new List<csapat>();
             foreach (var sor in File.ReadAllLines("fifa.txt").Skip(1))
             {
-                Csapatok.Add(new Csapat(sor));
+                csapatok.Add(new csapat(sor));
             }
-             Console.WriteLine($"3.feladat: A fifázgatásban  {Csapatok.Count} csapat szerepel.");
+
+
+            //3.feladat
+
+            Console.WriteLine($"3. feladat: A világranglistán {csapatok.Count} csapat szerepel");
+
+
+            //4.feladat
+            Console.WriteLine($"4. feladat: A csapatok átlagos pontszáma: {csapatok.Average(x => x.pont)} pont");
+
+            //5.feladat
+
+            Console.WriteLine($"5. feladat: A legtöbbet javító csapat:");
+            csapat legtöbbjav = csapatok.OrderBy(x => x.valtozas).Last()
+                ;
+            Console.WriteLine($"\tHelyezés: {legtöbbjav.helyezés}");
+            Console.WriteLine($"\tCsapat: {legtöbbjav.csnev}");
+
+            Console.WriteLine($"\tPontszám: {legtöbbjav.pont}");
+
+            //6.feladat
+            Console.WriteLine($"6. feladat: A csapatok között {(csapatok.Any(x => x.csnev == "Magyarország") ? "van" : "nincs")}");
+
+            //7.feladat
+
+
+            Console.WriteLine($"7. feladat: Statisztika:");
+
+            csapatok.GroupBy(x => x.valtozas)
+                .Select(g => new { valt = g.Key, db = g.Count() })
+                .Where(x => x.db > 1).ToList()
+                .ForEach(x => Console.WriteLine($"{x.valt} helyet vátozott: {x.db} csapat"));
         }
     }
+
+    
 }
